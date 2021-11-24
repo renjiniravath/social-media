b1428700ceeee0a74096eb9486e59550a5c07e6f
Renji Joseph Sabu <renjiniravath@gmail.com>
2021-11-24 05:06:09 -0500

    only alphanumeric strings accepted as username

--- src/components/login.js
+++ src/components/login.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { Form, Input, FormGroup, Label, Button } from "reactstrap";
+import {
+  Form,
+  Input,
+  FormGroup,
+  Label,
+  Button,
+  FormFeedback,
+} from "reactstrap";
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       usernameInput: "",
+      invalidInput: false,
     };
   }
 
@@ -15,19 +23,39 @@ class Login extends React.Component {
     });
   };
 
+  submitUsername = () => {
+    const username = this.state.usernameInput;
+    if (!/^[a-zA-Z0-9]+$/.test(username)) {
+      this.setState({
+        usernameInput: "",
+        invalidInput: true,
+      });
+      return;
+    }
+    this.props.setUsername(username);
+  };
+
   render() {
-    const { usernameInput } = this.state;
+    const { usernameInput, invalidInput } = this.state;
     return (
       <div className="app-body">
-        <Form onSubmit={() => this.props.setUsername(usernameInput)}>
-          <FormGroup>
+        <Form>
+          <FormGroup className="position-relative">
             <Label for="username">Enter a Username</Label>
             <Input
               id="username"
               onChange={(event) => this.changeInput(event.target.value)}
               value={usernameInput}
+              invalid={invalidInput}
             />
-            <Button color="primary" type="submit">
+            <FormFeedback tooltip>
+              Only alphanumeric strings are accepted as username
+            </FormFeedback>
+            <Button
+              color="primary"
+              type="button"
+              onClick={() => this.submitUsername()}
+            >
               Proceed
             </Button>
           </FormGroup>
